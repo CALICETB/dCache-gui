30c1064544cd4a01da263f360539118637ef4200
ebrianne <eldwan.brianne@desy.de>
2016-06-03 18:10:15 +0200

    Starting message

--- src/dCache-gui.cpp
+++ src/dCache-gui.cpp
@@ -1,11 +1,23 @@
 #include "dCacheMainWindow.h"
 #include <QApplication>
+#include <iostream>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    dCacheMainWindow w;
-    w.show();
 
-    return a.exec();
+	cout << "#####################################" << endl;
+	cout << "#                                   #" << endl;
+	cout << "#            dCache-gui             #" << endl;
+	cout << "#    Developed by E.Brianne (DESY)  #" << endl;
+	cout << "#     GUI to copy files on dCache   #" << endl;
+	cout << "#                                   #" << endl;
+	cout << "#####################################" << endl;
+
+	QApplication a(argc, argv);
+	dCacheMainWindow w;
+	w.show();
+
+	return a.exec();
 }

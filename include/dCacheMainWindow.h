6a434f46ee75c9cd60601ac9902a65c1107ccf0d
ebrianne <eldwan.brianne@desy.de>
2016-06-03 19:09:15 +0200

    Documentation of GUI class

--- include/dCacheMainWindow.h
+++ include/dCacheMainWindow.h
@@ -1,9 +1,33 @@
+/**
+ * @file
+ * @author  Eldwan Brianne <eldwan.brianne@desy.de>
+ * @version 1.0
+ *
+ * @section LICENSE
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details at
+ * https://www.gnu.org/copyleft/gpl.html
+ *
+ *
+ * @brief The GUI class to interact with the user
+ */
+
 #ifndef DCACHEMAINWINDOW_H
 #define DCACHEMAINWINDOW_H
 
+//-- Qt headers
 #include <QMainWindow>
 #include <QTimer>
 
+//-- Objects
 #include "Logger.h"
 #include "dCacheTools.h"
 #include "ProxyTools.h"
@@ -17,42 +41,153 @@ class dCacheMainWindow : public QMainWindow
 	Q_OBJECT
 
 public:
+
+	/**
+	 * Constructor for the GUI.
+	 * @param parent Parent QWidget.
+	 */
 	explicit dCacheMainWindow(QWidget *parent = 0);
+
+	/**
+	 * Destructor for the GUI.
+	 */
 	~dCacheMainWindow();
 
+	/**
+	 * Initialization method
+	 */
 	void init();
 
+	/**
+	 * Signals
+	 */
 	signals :
-	//Signal to Logger
+
+	/**
+	 * Signal to be sent to the logger widget
+	 * @param type Type of logging message (INFO, WARNING, ERROR, DEBUG).
+	 * @param message Message to send to the logger
+	 * @return void
+	 */
 	void log(QString type, QString message);
+
+	/**
+	 * Signal to be sent to the Thread for configuration
+	 * @param InputDir Input directory or file to be copied/check on the dCache
+	 * @param BaseDir Base directory on the dCache i.e. tb-desy/native/desyAhcal2016
+	 * @param OutputDir Output directory on the dCache where to put the files to upload or check
+	 * @param type Type of files to upload (txt, slcio, raw or other type)
+	 * @param isSingleFile Boolean to notify the thread that it is a single file to upload
+	 * @return void
+	 */
 	void Configure_dCacheTool(QString InputDir, QString BaseDir, QString OutputDir, int type, bool isSingleFile);
 
+	/**
+	 * Private slots
+	 */
 	private slots:
+
+	/**
+	 * Method when the look for directory button is clicked
+	 * @return void
+	 */
 	void on_toolButton_clicked();
+
+	/**
+	 * Method when the Configure button is clicked
+	 * @return void
+	 */
 	void Configure();
+
+	/**
+	 * Method when the password for the proxy is asked
+	 * @return void
+	 */
 	void showPassword();
+
+	/**
+	 * Method to get the timeleft of the proxy
+	 * @param status Timeleft of the proxy
+	 * @return void
+	 */
 	void updateProxy(QString status);
+
+	/**
+	 * Method to update the GUI
+	 * @return void
+	 */
 	void updateMainWindow();
+
+	/**
+	 * Method to set a parameter when the Thread is running
+	 * @return void
+	 */
 	void ThreadRunning();
+
+	/**
+	 * Method to set a parameter when the Thread is stopped
+	 * @return void
+	 */
 	void ThreadStopped();
 
+	/**
+	 * Method when the Start Copy button is clicked
+	 * @return void
+	 */
 	void StartCopy();
+
+	/**
+	 * Method when the Stop Copy button is clicked
+	 * @return void
+	 */
 	void StopCopy();
+
+	/**
+	 * Method when the List Files button is clicked
+	 * @return void
+	 */
 	void ListFiles();
+
+	/**
+	 * Method when the Check Copy button is clicked
+	 * @return void
+	 */
 	void CheckCopy();
 
+	/**
+	 * Method Method when the Exit button is clicked
+	 * @return void
+	 */
 	void Close();
 
+	/**
+	 * Private variables
+	 */
 	private:
-	Ui::dCacheMainWindow *ui;
-	Logger *m_logger;
-	dCacheTools *m_tools;
-	ProxyTools *m_proxy;
-	QTimer *timer;
 
-	QString InputDir, BaseDir, OutputDir;
-	bool isLabview, isEUDAQ, isLED, isRaw, isOther, isSingleFile, m_running;
-	int timeleft, timertime, type;
+	Ui::dCacheMainWindow *ui; /**< UI pointer */
+
+	Logger *m_logger; /**< Logger widget object */
+	dCacheTools *m_tools; /**< dCacheTools object */
+	ProxyTools *m_proxy; /**< Proxy object */
+
+	QTimer *timer; /**< Timer object */
+
+	QString InputDir; /**< Variable to store the input directory/file */
+	QString BaseDir;  /**< Variable to store the base directory */
+	QString OutputDir; /**< Variable to store the output directory */
+
+	bool isLabview; /**< Boolean is set to true if file is text file */
+	bool isEUDAQ; /**< Boolean is set to true if file is slcio file */
+	bool isLED; /**< Boolean is set to true if file is text file */
+	bool isRaw; /**< Boolean is set to true if file is raw binary file */
+	bool isOther; /**< Boolean is set to true if file is another type of file */
+	bool isSingleFile; /**< Boolean is set to true if upload a single file */
+	bool m_running; /**< Boolean set to true if dCacheTools Thread is running */
+
+	int timeleft; /**< Variable to store the timeleft of the proxy */
+	int timertime; /**< Variable to set the timer to update the GUI */
+	int type; /**< Variable to store the type of the file (1 text files, 2 slcio files, 3 raw binary and 4 for other types) */
 
 };
 
--- src/dCacheMainWindow.cpp
+++ src/dCacheMainWindow.cpp
@@ -1,21 +1,28 @@
 #include "dCacheMainWindow.h"
 #include "ui_dCacheMainWindow.h"
 
+//-- Qt headers
 #include <QInputDialog>
 #include <QFileDialog>
 #include <QEventLoop>
+
+//--------------------------------------------------------------------------------------------------------------
 
 dCacheMainWindow::dCacheMainWindow(QWidget *parent) :
 QMainWindow(parent),
 ui(new Ui::dCacheMainWindow)
 {
 	ui->setupUi(this);
-	this->setWindowTitle("dCache GUI");
+	this->setWindowTitle("dCache GUI"); //set title of the GUI
 
-	m_logger = new Logger();
-	m_proxy = new ProxyTools();
+	m_logger = new Logger(); //init Logger Widget
+	m_proxy = new ProxyTools(); //init Proxy handler object
 
-	timertime = 500;
+	timertime = 500; //timer to update the GUI in ms
+
+	/**
+	 * Initialization of the member variables
+	 */
 	timeleft = 0;
 	type = -1;
 	InputDir = "";
@@ -30,16 +37,28 @@ ui(new Ui::dCacheMainWindow)
 	type = -1;
 	m_running = false;
 
+	/**
+	 * Connection of the signals and slots
+	 */
+
+	//Connect the GUI to forward message to the logger
 	connect(this, SIGNAL(log(QString,QString)), m_logger, SLOT(Log(QString,QString)));
 
+	//Connect the proxy object to forward message to the logger
 	connect(m_proxy, SIGNAL(log(QString,QString)), m_logger, SLOT(Log(QString,QString)));
+	//Connect the proxy to the GUI to prompt window when password is asked
 	connect(m_proxy, SIGNAL(PasswordRequired()), this, SLOT(showPassword()));
+	//Connect the proxy to the GUI to update the time of the proxy left
 	connect(m_proxy, SIGNAL(ProxyStatus(QString)), this, SLOT(updateProxy(QString)));
 
+	//Logging starting
 	emit log("MESSAGE", "dCache-GUI started");
 
-	m_logger->show();
+	m_logger->show();//Show the logger widget
 
+	/**
+	 * Connection of the different UI objects to slots
+	 */
 	connect(ui->StartProxy, SIGNAL(clicked(bool)), m_proxy, SLOT(StartProxy()));
 	connect(ui->CheckProxy, SIGNAL(clicked(bool)), m_proxy, SLOT(CheckProxy()));
 
@@ -50,6 +69,9 @@ ui(new Ui::dCacheMainWindow)
 	connect(ui->ListFiles, SIGNAL(clicked(bool)), this, SLOT(ListFiles()));
 	connect(ui->Exit, SIGNAL(clicked(bool)), this, SLOT(Close()));
 
+	/**
+	 * Initialization of the state of the buttons and input text
+	 */
 	ui->StartCopy->setEnabled(false);
 	ui->StopCopy->setEnabled(false);
 	ui->ListFiles->setEnabled(false);
@@ -59,38 +81,62 @@ ui(new Ui::dCacheMainWindow)
 	ui->OutputDir->setReadOnly(true);
 	ui->Configure->setEnabled(false);
 
+	/**
+	 * Initialization of some labels on the GUI
+	 */
 	ui->ProxyValid_label->setText("<font color='Red'>Check Proxy!</font>");
-	ui->BaseDir->setText("tb-desy/native/desyAhcal2016/AHCAL_Testbeam_Raw_May_2016");
+	//Native base directory for calice
+	ui->BaseDir->setText("tb-desy/native/");
 	ui->LastRun->setText(".....");
 
+	/**
+	 * Setup the timer and connect the timeout signal to the slot that updates the GUI
+	 */
 	timer = new QTimer();
 	connect(timer, SIGNAL(timeout()), this, SLOT(updateMainWindow()));
-	timer->start(timertime);
+	timer->start(timertime);//Updates every 500 ms
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 dCacheMainWindow::~dCacheMainWindow()
 {
 	delete ui;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::init()
 {
+	/**
+	 * Initialization of the QThread that handles dCache operations (Check, Listing and Copy)
+	 * Connection of the different signals of the Thread to GUI slots
+	 * emit the configuration of the directories to the QThread
+	 */
 	m_tools = new dCacheTools();
 	connect(this, SIGNAL(Configure_dCacheTool(QString, QString, QString, int, bool)), m_tools, SLOT(Configure(QString, QString, QString, int, bool)));
 	connect(m_tools, SIGNAL(log(QString,QString)), m_logger, SLOT(Log(QString,QString)));
+	//Direct Connection to receive the signal before the end of the QThread
 	connect(m_tools, SIGNAL(finished()), this, SLOT(ThreadStopped()), Qt::DirectConnection);
 	connect(m_tools, SIGNAL(started()), this, SLOT(ThreadRunning()), Qt::DirectConnection);
 
 	emit Configure_dCacheTool(InputDir, BaseDir, OutputDir, type, isSingleFile);
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::Configure()
 {
-	InputDir = ui->InputDir->text();
-	BaseDir = ui->BaseDir->text();
-	OutputDir = ui->OutputDir->text();
+	/**
+	 * Configure button
+	 * Get the different parameters and set them to their assigned members
+	 */
 
-	if(isSingleFile == true)
+	InputDir = ui->InputDir->text();//Input file or directory
+	BaseDir = ui->BaseDir->text();//Base directory
+	OutputDir = ui->OutputDir->text();//Output directory
+
+	if(isSingleFile == true)//Case Single file
 	{
 		if((!InputDir.isEmpty() && !BaseDir.isEmpty()))
 		{
@@ -111,7 +157,7 @@ void dCacheMainWindow::Configure()
 			ui->Configure->setEnabled(false);
 		}
 		else
-			emit log("WARNING", "Settings are missing");
+			emit log("WARNING", "Settings are missing");//Input and Base directory missing
 	}
 	else
 	{
@@ -122,6 +168,10 @@ void dCacheMainWindow::Configure()
 		}
 		else
 		{
+			/**
+			 * Case when the configuration is fine
+			 */
+
 			emit log("MESSAGE", "dCache Copy Settings :");
 			if(!OutputDir.isEmpty())
 				emit log("MESSAGE", QString("Input Directory : %1\n\t"
@@ -141,8 +191,14 @@ void dCacheMainWindow::Configure()
 	}
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::showPassword()
 {
+	/**
+	 * Show an input Dialog box to enter the proxy password
+	 */
+
 	bool ok;
 	QInputDialog* inputDialog = new QInputDialog();
 	inputDialog->setOptions(QInputDialog::NoButtons);
@@ -152,42 +208,58 @@ void dCacheMainWindow::showPassword()
 
 	if(ok && !password.isEmpty())
 	{
-		m_proxy->setPassword(password);
+		m_proxy->setPassword(password);//send the password to the process that creates the grid proxy
 	}
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::updateProxy(QString status)
 {
-	//emit log("INFO", status);
+	/**
+	 * Get the timeleft of the proxy
+	 */
+
 	timeleft = status.toInt();
 
-	this->updateMainWindow();
+	this->updateMainWindow();//update the GUI
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void dCacheMainWindow::on_toolButton_clicked()
 {
+	/**
+	 * Method to prompt a QFileDialog when searching for a directory or file to upload on the dCache
+	 */
+
 	QString file;
 
+	//Case single file upload
 	if(isSingleFile)
 	{
+		//LED or Labview files
 		if(type == 1)
 			file = QFileDialog::getOpenFileName(this, tr("Choose File"),
 					"/home/calice",
 					tr("Labview file : *.txt"));
-
+		//slcio files
 		else if(type == 2)
 			file = QFileDialog::getOpenFileName(this, tr("Choose File"),
 					"/home/calice",
 					tr("EUDAQ file : *.slcio"));
+		//raw binary files
 		else if(type == 3)
 			file = QFileDialog::getOpenFileName(this, tr("Choose File"),
 					"/home/calice",
 					tr("Raw file : *.raw"));
+		//other files
 		else
 			file = QFileDialog::getOpenFileName(this, tr("Choose File"),
 					"/home/calice",
 					tr("Any type : *.*"));
 	}
+	//Case for directories
 	else
 	{
 		file = QFileDialog::getExistingDirectory(this, tr("Choose Directory"),
@@ -196,11 +268,18 @@ void dCacheMainWindow::on_toolButton_clicked()
 				| QFileDialog::DontResolveSymlinks);
 	}
 
-	ui->InputDir->setText(file);
+	ui->InputDir->setText(file);//Set the path into the GUI
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void dCacheMainWindow::StartCopy()
 {
+	/**
+	 * Start Copy button clicked
+	 * Setup the different buttons and start the QThread
+	 */
+
 	ui->StartCopy->setEnabled(false);
 	ui->StopCopy->setEnabled(true);
 	ui->Configure->setEnabled(false);
@@ -211,20 +290,34 @@ void dCacheMainWindow::StartCopy()
 	ui->BaseDir->setReadOnly(true);
 	ui->OutputDir->setReadOnly(true);
 
-	this->init();
-	m_tools->setFlags(true, false, false);
-	m_tools->start();
+	this->init();//Initialization of the QThread
+	m_tools->setFlags(true, false, false);//Set flags (Copy, Check, List)
+	m_tools->start();//Start the QThread
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void dCacheMainWindow::ListFiles()
 {
-	this->init();
-	m_tools->setFlags(false, false, true);
-	m_tools->start();
+	/**
+	 * List button clicked
+	 * Start the QThread
+	 */
+
+	this->init();//Initialization of the QThread
+	m_tools->setFlags(false, false, true);//Set flags (Copy, Check, List)
+	m_tools->start();//Start the QThread
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void dCacheMainWindow::StopCopy()
 {
+	/**
+	 * Stop Copy button clicked
+	 * Setup the GUI buttons and send stop signal to the QThread
+	 */
+
 	ui->StartCopy->setEnabled(true);
 	ui->StopCopy->setEnabled(false);
 	ui->Configure->setEnabled(true);
@@ -235,41 +328,68 @@ void dCacheMainWindow::StopCopy()
 	ui->BaseDir->setReadOnly(false);
 	ui->OutputDir->setReadOnly(false);
 
-	emit log("MESSAGE", "Stop Copy");
-	m_tools->setStopFlag(true);
+	emit log("MESSAGE", "Stop Copy");//Logging
+	m_tools->setStopFlag(true);//send stop signal to the QThread
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void dCacheMainWindow::CheckCopy()
 {
-	this->init();
-	m_tools->setFlags(false, true, false);
-	m_tools->start();
+	/**
+	 * Check button clicked
+	 * Set the flags and start the QThread
+	 */
+
+	this->init();//Initialization of the QThread
+	m_tools->setFlags(false, true, false);//Set flags (Copy, Check, List)
+	m_tools->start();//Start the QThread
 }
+
+//--------------------------------------------------------------------------------------------------------------
 
 void dCacheMainWindow::Close()
 {
-	emit log("MESSAGE", "dCache-GUI exiting");
+	/**
+	 * Exit button clicked
+	 * Clean and exit the GUI
+	 */
+
+	emit log("MESSAGE", "dCache-GUI exiting");//Logging
 
 	if(timeleft != 0)
-		m_proxy->DestroyProxy(timeleft);
+		m_proxy->DestroyProxy(timeleft);//Destroy the proxy
 
 	if(m_running)
-		m_tools->quit();
+		m_tools->quit();//Stop the QThread if running
 
+	//Wait 2 secs
 	QEventLoop loop;
 	QTimer::singleShot(2000, &loop, SLOT(quit()));
 	loop.exec();
 
+	//Close the logger widget and GUI
 	m_logger->close();
 	this->close();
 
+	//Cleaning
 	delete m_tools;
 	delete m_proxy;
 	delete m_logger;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::updateMainWindow()
 {
+	/**
+	 * Updates the GUI
+	 * Set the different buttons for different cases
+	 */
+
+	/**
+	 * Check the type of files and if single file upload
+	 */
 	isSingleFile = ui->SingleCheck->isChecked();
 	isLabview = ui->LabviewCheck->isChecked();
 	isEUDAQ = ui->EUDAQCheck->isChecked();
@@ -277,6 +397,7 @@ void dCacheMainWindow::updateMainWindow()
 	isRaw = ui->RawCheck->isChecked();
 	isOther = ui->OtherCheck->isChecked();
 
+	//Define the type
 	if(isLabview || isLED)
 		type = 1;
 	if(isEUDAQ)
@@ -286,6 +407,7 @@ void dCacheMainWindow::updateMainWindow()
 	if(isOther)
 		type = 4;
 
+	//Case no timeleft in the grid proxy
 	if(timeleft == 0)
 	{
 		ui->StartCopy->setEnabled(false);
@@ -302,6 +424,7 @@ void dCacheMainWindow::updateMainWindow()
 	}
 	else
 	{
+		//If type defined user can enter directory parameters
 		if(type != -1)
 		{
 			ui->toolButton->setEnabled(true);
@@ -310,6 +433,7 @@ void dCacheMainWindow::updateMainWindow()
 			ui->OutputDir->setReadOnly(false);
 		}
 
+		//Case QThread is running avoiding interactions with the user
 		if(!m_running)
 		{
 			ui->Configure->setEnabled(true);
@@ -325,6 +449,9 @@ void dCacheMainWindow::updateMainWindow()
 
 		ui->CheckProxy->setEnabled(true);
 
+		/**
+		 * Updates the timeleft on the GUI
+		 */
 		timeleft = timeleft - timertime*2/1000;
 		int hours = timeleft/3600;
 		int minutes = timeleft%3600/60;
@@ -336,12 +463,22 @@ void dCacheMainWindow::updateMainWindow()
 	}
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::ThreadRunning()
 {
+	/**
+	 * Case QThread is running
+	 */
 	m_running = true;
 }
 
+//--------------------------------------------------------------------------------------------------------------
+
 void dCacheMainWindow::ThreadStopped()
 {
+	/**
+	 * Case QThread is stopped
+	 */
 	m_running = false;
 }

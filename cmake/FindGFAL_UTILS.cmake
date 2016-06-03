6136b5ee4bee66758d209d9097a9fee303383026
ebrianne <eldwan.brianne@desy.de>
2016-06-04 01:51:04 +0200

    Added Module to find gfal-ls and gfal-copy needed Set the check by -DIS_DEVEL=OFF

---
+++ cmake/FindGFAL_UTILS.cmake
@@ -0,0 +1,23 @@
+#  CMake used to find if gfal-utils are installed
+#  Once done this will define
+#  GFAL_UTILS_FOUND - System has GFAL_UTILS
+
+find_program (GFAL_LS NAMES gfal-ls 
+					  PATHS /usr/bin )
+
+find_program (GFAL_COPY NAMES gfal-copy 
+					  PATHS /usr/bin )
+
+include(FindPackageHandleStandardArgs)
+# handle the QUIETLY and REQUIRED arguments and set GFAL_UTILS_FOUND to TRUE
+# if all listed variables are TRUE
+find_package_handle_standard_args(GFAL_UTILS  DEFAULT_MSG
+                                  GFAL_LS GFAL_COPY)
+                                  
+                                  
+MESSAGE( STATUS "" )
+MESSAGE( STATUS "---------------------------------------" )
+MESSAGE( STATUS "GFAL_LS                    = ${GFAL_LS}" )
+MESSAGE( STATUS "GFAL_COPY                = ${GFAL_COPY}" )
+MESSAGE( STATUS "---------------------------------------" )
+MESSAGE( STATUS "" )

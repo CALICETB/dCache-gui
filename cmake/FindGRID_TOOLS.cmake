c9e83dbab2d3ab4ef6b076be28043f3a606d542b
ebrianne <eldwan.brianne@desy.de>
2016-06-04 02:27:20 +0200

    Renamed GFAL_UTILS to GRID_TOOLS Check also for voms-proxy-init and voms-proxy-info

--- cmake/FindGRID_TOOLS.cmake
+++ cmake/FindGRID_TOOLS.cmake
@@ -1,23 +1,31 @@
 #  CMake used to find if gfal-utils are installed
 #  Once done this will define
-#  GFAL_UTILS_FOUND - System has GFAL_UTILS
+#  GRID_TOOLS_FOUND - System has GFAL_UTILS and VOMS
 
 find_program (GFAL_LS NAMES gfal-ls 
 					  PATHS /usr/bin )
 
 find_program (GFAL_COPY NAMES gfal-copy 
 					  PATHS /usr/bin )
-
+					  
+find_program (VOMS_INIT NAMES voms-proxy-init 
+					  PATHS /usr/bin )
+					  				  
+find_program (VOMS_INFO NAMES voms-proxy-info
+					  PATHS /usr/bin )
+					  
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set GFAL_UTILS_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(GFAL_UTILS  DEFAULT_MSG
-                                  GFAL_LS GFAL_COPY)
+find_package_handle_standard_args(GRID_TOOLS  DEFAULT_MSG
+                                  GFAL_LS GFAL_COPY VOMS_INIT VOMS_INFO)
                                   
                                   
 MESSAGE( STATUS "" )
 MESSAGE( STATUS "---------------------------------------" )
 MESSAGE( STATUS "GFAL_LS                    = ${GFAL_LS}" )
 MESSAGE( STATUS "GFAL_COPY                  = ${GFAL_COPY}" )
+MESSAGE( STATUS "VOMS_INIT                  = ${VOMS_INIT}" )
+MESSAGE( STATUS "VOMS_INFO                  = ${VOMS_INFO}" )
 MESSAGE( STATUS "---------------------------------------" )
 MESSAGE( STATUS "" )
